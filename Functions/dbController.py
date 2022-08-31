f28dd9d236459f8763c66ea9b32423ea45e4ee0f
Merven Thomas <mervthom21@gmail.com>
2022-08-31 11:26:49 -0500

    Fixed walmart bug

--- Functions/dbController.py
+++ Functions/dbController.py
@@ -17,5 +17,9 @@ def executeReadQuery(connection, query):
   result = cursor.fetchall()
   return result
 
+def clearDataBase(connection):
+  executeQuery(connection, "DROP TABLE IF EXISTS products")
+
 path = "C:/Users/Merven Thomas/Desktop/PriceComparer/Database/database.sql"
 db = connect(path)
+executeQuery(db, "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, price INTEGER, name TEXT, link TEXT);")
--- Functions/walmart.py
+++ Functions/walmart.py
@@ -2,33 +2,27 @@ from bs4 import BeautifulSoup as soup
 import requests
 
 def Walmart(search):
-  URL = "https://www.walmart.com/search?q=" + search
+  URL = "https://www.walmart.com/search?q=" + search + "&facet=fulfillment_method%3AShipping"
   headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Accept-Encoding": "gzip, deflate, br", "Upgrade-Insecure-Requests": "1"}
 
   outputList = []
+  
+  page = requests.get(URL, headers=headers)
+  soup1 = soup(page.content, "html.parser")
 
-  try:
-      page = requests.get(URL, headers=headers)
-      soup1 = soup(page.content, "html.parser")
-  except:
-      print("Could not connect to walmart.com")
-      return
-
-  products = soup1.findAll("div",{"class":"mb1 ph1 pa0-xl bb b--near-white w-33"})
+  products = soup1.findAll("div",{"class":"mb1 ph1 pa0-xl bb b--near-white w-25"})
     
   for product in products:
-    try:
-      productName = product.div.div.div.div.next_sibling.div.next_sibling.text
-      productPrice = product.div.div.div.div.next_sibling.div.div.text
-      productPrice = productPrice.replace(",", "")
-      productPrice = float(productPrice[1:])
-      productLink = product.div.div.a["href"]
-      productLink = "https://www.walmart.com" + productLink
+    
+    productName = product.div.div.div.div.next_sibling.div.next_sibling.text
+    productPrice = product.div.div.div.div.next_sibling.div.div.text
+    productPrice = productPrice.replace(",", "")
+    productPrice = float(productPrice[1:])
+    productLink = product.div.div.a["href"]
+    productLink = "https://www.walmart.com" + productLink
 
-      newProduct = {"name":productName,"price":productPrice,"link":productLink}
-      outputList.append(newProduct)
-    except:
-      pass
+    newProduct = {"name":productName,"price":productPrice,"link":productLink}
+    outputList.append(newProduct)
 
   return outputList
  
